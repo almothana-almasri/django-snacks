cc9ded4a1e9f3feb83da9982443c20e18f3ffcc3
almothana-almasri <mothana.almasri.99@gmail.com>
2023-07-10 12:09:52 +0000

    Added tailwind

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+# from django.urls.generic import TemplateViews
 # Create your views here.
+
+# class HomeView(TemplateView):
+#     template_name = 'home.html'
+
+def home(request):
+    return render(request, 'snacks/home.html')
+
+def about(request):
+    return render(request, 'snacks/about.html')
