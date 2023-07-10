1504ce398a7bd508fbe7eb5ca9e7687694711fed
almothana-almasri <mothana.almasri.99@gmail.com>
2023-07-10 15:23:28 +0000

    Added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [],
+  darkMode: true,
   content: [],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
